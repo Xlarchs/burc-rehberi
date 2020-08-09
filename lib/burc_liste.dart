6b8eab49c70aeec8260bf74f0487a602ec75da71
ilkerogtc <ilkerogtc@gmail.com>
2020-08-10 01:28:18 +0300

    veri kaynaklarını hazırlama bitti

--- lib/burc_liste.dart
+++ lib/burc_liste.dart
@@ -1,8 +1,33 @@
+import 'package:burc_rehberi/models/burc.dart';
+import 'package:burc_rehberi/utils/strings.dart';
 import 'package:flutter/material.dart';
 
 class BurcListesi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List<Burc> tumBurclar = veriKaynaginiHazirla();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Burç Rehberi"),
+      ),
+      body: Container(),
+    );
+  }
+
+  List<Burc> veriKaynaginiHazirla() {
+    List<Burc> burclar = [];
+    for (var i = 0; i < 12; i++) {
+      String kucukResim = Strings.BURC_ADLARI[i].toLowerCase() + "${i + 1}.png";
+      String buyukResim =
+          Strings.BURC_ADLARI[i].toLowerCase() + "_buyuk${i + 1}.png";
+
+      Burc eklenecekBurc = Burc(
+          Strings.BURC_ADLARI[i],
+          Strings.BURC_TARIHLERI[i],
+          Strings.BURC_GENEL_OZELLIKLERI[i],
+          kucukResim,
+          buyukResim);
+      burclar.add(eklenecekBurc);
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
+import 'package:burc_rehberi/burc_liste.dart';
 import 'package:flutter/material.dart';
 
-void Main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Burç Rehberi",
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: ,
+      home: BurcListesi(),
     );
   }
 }
