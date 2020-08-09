902c3178f3148646e190d9b048281dbbb59b0108
ilkerogtc <ilkerogtc@gmail.com>
2020-08-10 00:53:21 +0300

    resim ve string dosyası eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+void Main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Burç Rehberi",
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: ,
+    );
+  }
+}
