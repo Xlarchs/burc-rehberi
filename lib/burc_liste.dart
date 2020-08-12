c0488ee4ffdb4dbfb172cb129062d6b1f1b23f3e
ilkerogtc <ilkerogtc@gmail.com>
2020-08-12 23:18:45 +0300

    burçlar ListTile ile listelendi

--- lib/burc_liste.dart
+++ lib/burc_liste.dart
@@ -2,15 +2,17 @@ import 'package:burc_rehberi/models/burc.dart';
 import 'package:burc_rehberi/utils/strings.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class BurcListesi extends StatelessWidget {
+  List<Burc> tumBurclar;
   @override
   Widget build(BuildContext context) {
-    List<Burc> tumBurclar = veriKaynaginiHazirla();
+    tumBurclar = veriKaynaginiHazirla();
     return Scaffold(
       appBar: AppBar(
         title: Text("Burç Rehberi"),
       ),
-      body: Container(),
+      body: listeyiHazirla(),
     );
   }
 
@@ -29,5 +31,47 @@ class BurcListesi extends StatelessWidget {
           buyukResim);
       burclar.add(eklenecekBurc);
     }
+    return burclar;
+  }
+
+  Widget listeyiHazirla() {
+    return ListView.builder(
+      itemBuilder: (context, index) => tekSatirBurcWidget(context, index),
+      itemCount: tumBurclar.length,
+    );
+  }
+
+  Widget tekSatirBurcWidget(context, index) {
+    Burc listeyeEklenenBurclar = tumBurclar[index];
+    return Card(
+      elevation: 4,
+      child: ListTile(
+        leading: Image.asset(
+          "images/" + listeyeEklenenBurclar.burcKucukResim,
+          width: 64,
+          height: 64,
+        ),
+        title: Text(
+          listeyeEklenenBurclar.burcAdi,
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+            color: Colors.pink.shade500,
+          ),
+        ),
+        subtitle: Text(
+          listeyeEklenenBurclar.burcTarihi,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w400,
+            color: Colors.black87,
+          ),
+        ),
+        trailing: Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.pink,
+        ),
+      ),
+    );
   }
 }
